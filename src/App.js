dab770b0e4026748536b7d29a586a2735280e8de
Pinar Arslan <99140556+pinnotr@users.noreply.github.com>
2025-05-16 14:31:45 +0300

    feat: Set up initial routing structure for the application
    - Added routes for Login and Register pages
    - Integrated Layout component for nested routes
    - Defined admin routes: students, teachers, courses
    - Included paths for user profile, user detail, course editing, and course creation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Account/Login';
 import Register from './Pages/Account/Register';
-import StudentDashboard from './Pages/Student/Styles/StudentDashboard';
+import Layout from './Layout/Layout';
+import AdminCoursePage from './Pages/Admin/AdminCoursePage';
+import AdminStudentPage from './Pages/Admin/AdminStudentPage';
+import AdminTeacherPage from './Pages/Admin/AdminTeacherPage';
+import CourseEditForm from './Components/CourseEditForm';
+import ProfilePage from './Components/ProfilePage';
+import UserDetail from './Components/UserDetail';
+import AddCourse from './Components/AddCourse';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Register" element={<Register />} />
-      <Route path="/StudentDashboard" element={<StudentDashboard />} />
-    </Routes>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/register" element={<Register />} />
+          <Route path='/admin/addcourse' element={<AddCourse />} />
+          <Route path="/userdetail/:userId" element={<UserDetail />} />
+          <Route path="/profilepage" element={<ProfilePage />} />
+          <Route path="/admin/courseeditform/:courseId" element={<CourseEditForm />} />
+          <Route path="/admin/teachers" element={<AdminTeacherPage />} />
+          <Route path="/admin/students" element={<AdminStudentPage />} />
+          <Route path='/admin/courses' element={<AdminCoursePage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
